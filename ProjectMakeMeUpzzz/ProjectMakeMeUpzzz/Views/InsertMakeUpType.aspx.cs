1f4fc2439a8270a997c2f83d9e17e54fe8ed794f
kvnnh <khermanto01@gmail.com>
2024-06-12 17:11:13 +0700

    insert update makeup

---
+++ ProjectMakeMeUpzzz/ProjectMakeMeUpzzz/Views/InsertMakeUpType.aspx.cs
@@ -0,0 +1,47 @@
+﻿using ProjectMakeMeUpzzz.Controllers;
+using ProjectMakeMeUpzzz.Helpers;
+using ProjectMakeMeUpzzz.Models;
+using ProjectMakeMeUpzzz.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+
+namespace ProjectMakeMeUpzzz.Views
+{
+    public partial class InsertMakeUpType : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //if (Session["user"] == null)
+            //{
+            //    Response.Redirect("~/Views/Home.aspx");
+            //}
+        }
+
+        protected void submitMakeUpType_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                String typeName = txt_Name.Text;
+                Response<MakeupType> response = MakeupTypeController.InsertMakeupType(typeName);
+                if (response.IsSuccess)
+                {
+                    Response.Redirect("~/Views/ManageMakeUp.aspx");
+                }
+
+                lbl_Error.Text = response.Message;
+                lbl_Error.Visible = true;
+
+            }
+            catch (Exception ex)
+            {
+                lbl_Error.Text = ex.Message;
+                lbl_Error.Visible = true;
+            }
+        }
+    }
+}
---
+++ ProjectMakeMeUpzzz/ProjectMakeMeUpzzz/Views/UpdateMakeUpType.aspx.cs
@@ -0,0 +1,46 @@
+﻿using ProjectMakeMeUpzzz.Controllers;
+using ProjectMakeMeUpzzz.Helpers;
+using ProjectMakeMeUpzzz.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ProjectMakeMeUpzzz.Views
+{
+    public partial class UpdateMakeUpType : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response<MakeupType> response = MakeupTypeController.GetMakeupTypeById(Convert.ToInt32(Request.QueryString["Id"]));
+            if (response.IsSuccess)
+            {
+                MakeupType makeupType = response.Payload;
+                txt_MakeUpTypeID.Text = makeupType.MakeupTypeID.ToString();
+                txt_MakeUpTypeName.Text = makeupType.MakeupTypeName;
+            }
+            else
+            {
+                Response.Write(response.Message);
+            }
+        }
+
+        protected void UpdateMakeUpTypeBtn_Click(object sender, EventArgs e)
+        {
+            string id = txt_MakeUpTypeID.Text;
+            string name = txt_MakeUpTypeName.Text;
+
+            Response<MakeupType> response = MakeupTypeController.UpdateMakeupType(id, name);
+
+            if (response.IsSuccess)
+            {
+                Response.Redirect("~/Views/ManageMakeUp.aspx");
+            }
+                lbl_Error.Text = response.Message;
+                lbl_Error.Visible = true;
+        }   
+
+    }
+}
