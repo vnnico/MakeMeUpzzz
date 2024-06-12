370a02a1ad3fe2ce7e77305c5313c12a5a9e3103
kvnnh <khermanto01@gmail.com>
2024-06-12 19:52:19 +0700

    insert update

--- ProjectMakeMeUpzzz/ProjectMakeMeUpzzz/Views/UpdateMakeUpType.aspx.cs
+++ ProjectMakeMeUpzzz/ProjectMakeMeUpzzz/Views/UpdateMakeUpType.aspx.cs
@@ -14,17 +14,31 @@ namespace ProjectMakeMeUpzzz.Views
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            Response<MakeupType> response = MakeupTypeController.GetMakeupTypeById(Convert.ToInt32(Request.QueryString["Id"]));
-            if (response.IsSuccess)
-            {
-                MakeupType makeupType = response.Payload;
-                txt_MakeUpTypeID.Text = makeupType.MakeupTypeID.ToString();
-                txt_MakeUpTypeName.Text = makeupType.MakeupTypeName;
-            }
-            else
-            {
-                Response.Write(response.Message);
-            }
+            //{
+            //    User user = (User)Session["user"];
+            //    if (user.UserRole != "admin")
+            //    {
+            //        Response.Redirect("~/Views/Home.aspx");
+            //    }
+            //    if (!Page.IsPostBack)
+            //    {
+                    Response<MakeupType> response = MakeupTypeController.GetMakeupTypeById(Convert.ToInt32(Request.QueryString["Id"]));
+                    if (response.IsSuccess)
+                    {
+                        MakeupType makeupType = response.Payload;
+                        txt_MakeUpTypeID.Text = makeupType.MakeupTypeID.ToString();
+                        txt_MakeUpTypeName.Text = makeupType.MakeupTypeName;
+                    }
+                    else
+                    {
+                        Response.Write(response.Message);
+                    }
+            //    }
+            //}
+            //else
+            //{
+            //    Response.Redirect("~/Views/Home.aspx");
+            //}
         }
 
         protected void UpdateMakeUpTypeBtn_Click(object sender, EventArgs e)
@@ -34,13 +48,14 @@ namespace ProjectMakeMeUpzzz.Views
 
             Response<MakeupType> response = MakeupTypeController.UpdateMakeupType(id, name);
 
-            if (response.IsSuccess)
-            {
-                Response.Redirect("~/Views/ManageMakeUp.aspx");
-            }
-                lbl_Error.Text = response.Message;
-                lbl_Error.Visible = true;
-        }   
+            // sengaja komen dulu soalnya ga bisa save database nya
+            //if (response.IsSuccess)
+            //{
+            //    Response.Redirect("~/Views/ManageMakeUp.aspx");
+            //}
+            lbl_Error.Text = response.Message;
+            lbl_Error.Visible = true;
+        }
 
     }
 }
