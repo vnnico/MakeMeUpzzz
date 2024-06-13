f0c96f7de51de8185b141fd4df99535ff33765ee
Trinata <159982772+NataaKuri@users.noreply.github.com>
2024-06-14 02:40:10 +0700

    checkout finish + role management

--- ProjectMakeMeUpzzz/ProjectMakeMeUpzzz/Views/InsertMakeUp.aspx.cs
+++ ProjectMakeMeUpzzz/ProjectMakeMeUpzzz/Views/InsertMakeUp.aspx.cs
@@ -13,28 +13,56 @@ namespace ProjectMakeMeUpzzz.Views
     public partial class InsertMakeUp : System.Web.UI.Page
         
     {
-
+        protected User user;
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (!Page.IsPostBack)
+
+            if (Session["user"] == null && Request.Cookies["user_auth"] == null)
             {
-                Response<List<MakeupType>> response = MakeupTypeController.GetAllMakeupTypes();
-                if (response.IsSuccess)
+                Response.Redirect("Login.aspx");
+            }
+            else
+            {
+                if (Session["user"] == null)
                 {
-                    MakeUpTypeIdDdl.DataSource = response.Payload;
-                    MakeUpTypeIdDdl.DataValueField = "MakeupTypeID";
-                    MakeUpTypeIdDdl.DataTextField = "MakeupTypeName";
-                    MakeUpTypeIdDdl.DataBind();
+                    int id = Convert.ToInt32(Request.Cookies["user_auth"].Value);
+                    Response<User> response = UserController.GetUserById(id);
+                    user = response.Payload;
+                    Session["user"] = user;
                 }
-                Response<List<MakeupBrand>> response2 = MakeupBrandController.GetAllMakeupBrands();
-                if (response2.IsSuccess)
+                else
                 {
-                    MakeUpBrandIdDdl.DataSource = response2.Payload;
-                    MakeUpBrandIdDdl.DataValueField = "MakeupBrandID";
-                    MakeUpBrandIdDdl.DataTextField = "MakeupBrandName";
-                    MakeUpBrandIdDdl.DataBind();
+                    user = (User)Session["user"];
+                }
+
+
+                if (user.UserRole != "admin")
+                {
+                    Response.Redirect("~/Views/Home.aspx");
+                    
+                }
+
+                if (!Page.IsPostBack)
+                {
+                    Response<List<MakeupType>> response = MakeupTypeController.GetAllMakeupTypes();
+                    if (response.IsSuccess)
+                    {
+                        MakeUpTypeIdDdl.DataSource = response.Payload;
+                        MakeUpTypeIdDdl.DataValueField = "MakeupTypeID";
+                        MakeUpTypeIdDdl.DataTextField = "MakeupTypeName";
+                        MakeUpTypeIdDdl.DataBind();
+                    }
+                    Response<List<MakeupBrand>> response2 = MakeupBrandController.GetAllMakeupBrands();
+                    if (response2.IsSuccess)
+                    {
+                        MakeUpBrandIdDdl.DataSource = response2.Payload;
+                        MakeUpBrandIdDdl.DataValueField = "MakeupBrandID";
+                        MakeUpBrandIdDdl.DataTextField = "MakeupBrandName";
+                        MakeUpBrandIdDdl.DataBind();
+                    }
                 }
             }
+            
         }
     
 
--- ProjectMakeMeUpzzz/ProjectMakeMeUpzzz/Views/ManageMakeUp.aspx.cs
+++ ProjectMakeMeUpzzz/ProjectMakeMeUpzzz/Views/ManageMakeUp.aspx.cs
@@ -13,41 +13,85 @@ namespace ProjectMakeMeUpzzz.Views
 {
     public partial class ManageMakeUp : System.Web.UI.Page
     {
-        
+
+        protected User user;
         protected void Page_Load(object sender, EventArgs e)
 
         {
-            Response<List<Makeup>> responses = MakeupController.GetAllMakeups();
-            InsertGrid.DataSource = responses.Payload;
-            InsertGrid.DataBind();
 
-            Response<List<MakeupBrand>> response2 = MakeupBrandController.GetAllMakeupBrands();
-            BrandGV.DataSource = response2.Payload;
-            BrandGV.DataBind();
+            if (Session["user"] == null && Request.Cookies["user_auth"] == null)
+            {
+                Response.Redirect("Login.aspx");
+            }
+            else
+            {
+                if (Session["user"] == null)
+                {
+                    int id = Convert.ToInt32(Request.Cookies["user_auth"].Value);
+                    Response<User> response = UserController.GetUserById(id);
+                    user = response.Payload;
+                    Session["user"] = user;
+                }
+                else
+                {
+                    user = (User)Session["user"];
+                }
 
-            Response<List<MakeupType>> responses3 = MakeupTypeController.GetAllMakeupTypes();
-            GridType.DataSource = responses3.Payload;
-            GridType.DataBind();
+
+                if (user.UserRole == "admin")
+                {
+
+                    Response<List<Makeup>> responses = MakeupController.GetAllMakeups();
+                    if (responses.IsSuccess)
+                    {
+
+                        InsertGrid.DataSource = responses.Payload;
+                        InsertGrid.DataBind();
+                    }
+
+                    Response<List<MakeupBrand>> response2 = MakeupBrandController.GetAllMakeupBrands();
+                    if (response2.IsSuccess)
+                    {
+
+                        BrandGV.DataSource = response2.Payload;
+                        BrandGV.DataBind();
+                    }
+
+
+                    Response<List<MakeupType>> responses3 = MakeupTypeController.GetAllMakeupTypes();
+                    if (responses3.IsSuccess)
+                    {
+
+                        GridType.DataSource = responses3.Payload;
+                        GridType.DataBind();
+                    }
+                } else
+                {
+                    Response.Redirect("~/Views/Home.aspx");
+                }
+            }
+
+
         }
 
 
 
-        
+
 
         protected void InsertGrid_RowEditing(object sender, GridViewEditEventArgs e)
         {
-           
-           GridViewRow row = InsertGrid.Rows[e.NewEditIndex];
+
+            GridViewRow row = InsertGrid.Rows[e.NewEditIndex];
             string id = row.Cells[0].Text.ToString();
             Response.Redirect("~/Views/UpdateMakeUp.aspx/?ID=" + id);
-        
+
         }
 
         protected void InsertGrid_RowDeleting(object sender, GridViewDeleteEventArgs e)
         {
             GridViewRow row = InsertGrid.Rows[e.RowIndex];
             int id = Convert.ToInt32(row.Cells[0].Text.ToString());
-           
+
 
             Response<Makeup> response = MakeupController.DeleteMakeup(id);
             if (response.IsSuccess)
@@ -58,7 +102,7 @@ namespace ProjectMakeMeUpzzz.Views
                     InsertGrid.DataSource = responses.Payload;
                     InsertGrid.DataBind();
                 }
-              
+
             }
         }
 
